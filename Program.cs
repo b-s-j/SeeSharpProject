1b8738c126210c68a76c6a011c44509a842c884d
Bern <paul25bern@gmail.com>
2024-06-03 23:08:51 +0800

    using list of array

--- Program.cs
+++ Program.cs
@@ -5,17 +5,21 @@ using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
+
 namespace seesharp_projects
 {
     class Program 
     {
         static void Main(string[] args)
         {
-            System.Console.Clear();
-            System.Console.WriteLine("Currency: {0:c}, 23.455");
-            System.Console.WriteLine("Pad with 0's: {0:d4}", 23);
-            System.Console.WriteLine("3 Decimals: {0:f3}", 23.45555);
-            System.Console.WriteLine("Commas: {0:n4}", 2355);
+           
+            var names = new string [] { "Scott", "Anna", "Felipe"};
+            names = [..names, "Anne", "Damian", "Bern",];
+
+            foreach (var name in names)
+            {
+                System.Console.WriteLine("" + name[^2]);
+            }
         }
     }
 }
