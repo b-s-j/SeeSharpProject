ff76c1dfb4e3af469b49279ea85f5334b50cadd0
Bern <paul25bern@gmail.com>
2024-06-05 18:47:20 +0800

    Reset - I am now reseting my testing environment project so that I can create more other project where I can practice more c# code program.

--- Program.cs
+++ Program.cs
@@ -12,67 +12,8 @@ namespace seesharp_projects
     {
         static void Main(string[] args)
         {
-            int number = -27; // You can change this number to any integer you want to convert
-            string words = NumberToWordConverter.NumberToWords(number);
-            Console.WriteLine(words);
-          
+           
+          //Reseting my Testing Environment project
         }
-
-        public static class NumberToWordConverter
-        {
-            private static readonly string[] UnitsMap = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
-            private static readonly string[] TensMap = { "Zero", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-
-            public static string NumberToWords(int number)
-            {
-                if (number == 0)
-                    return UnitsMap[0];
-
-                if (number < 0)
-                    return "Negative " + NumberToWords(Math.Abs(number));
-
-                var words = new StringBuilder();
-
-                words.Append(ProcessGroup(number / 1000000000, "Billion"));
-                number %= 1000000000;
-
-                words.Append(ProcessGroup(number / 1000000, "Million"));
-                number %= 1000000;
-
-                words.Append(ProcessGroup(number / 1000, "Thousand"));
-                number %= 1000;
-
-                words.Append(ProcessGroup(number, ""));
-
-                return words.ToString().Trim();
-            }
-
-            private static string ProcessGroup(int number, string scale)
-            {
-                if (number <= 0) return "";
-
-                var groupWords = new StringBuilder();
-
-                if (number >= 100)
-                {
-                    groupWords.Append(UnitsMap[number / 100] + " Hundred ");
-                    number %= 100;
-                }
-
-                if (number > 0)
-                {
-                    if (number < 20)
-                        groupWords.Append(UnitsMap[number]);
-                    else
-                    {
-                        groupWords.Append(TensMap[number / 10]);
-                        if ((number % 10) > 0)
-                            groupWords.Append("-" + UnitsMap[number % 10]);
-                    }
-                }
-                return groupWords.Append(" " + scale + " ").ToString();
-            }
-        }
-
     }
 }
