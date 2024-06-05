99146cfbe56ee5a2e4e588bb46f2027f2d873dd9
Bern <paul25bern@gmail.com>
2024-06-05 18:49:44 +0800

    The Right commiting
    This is another commit where as I am trying to
    commit in vs code because I see a lot of ways on
    how to commit.
    --Bern

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace seesharp_projects
            
           //Reseting my Testing Environment project
           System.Console.WriteLine("Hello Reset ");
+          System.Console.WriteLine("trying different commits on vs code so I can understand how the right commit works in different ways:");
         }
     }
 }
