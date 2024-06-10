ac4f662e9f909c18f81dd023c4bab2cd01da73f7
Bern <paul25bern@gmail.com>
2024-06-10 19:21:07 +0800

    10-06-2024

--- obj/Debug/net8.0/seesharp-projects.AssemblyInfo.cs
+++ obj/Debug/net8.0/seesharp-projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47e1279252a2d9d26423a4f3acaac32a43bb73dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f12ccd33135639d534dd1501086ea0cd0d6a36a")]
 [assembly: System.Reflection.AssemblyProductAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
