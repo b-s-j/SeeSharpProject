7eb4332fadd8f91b451a6e0be754fe542219efb4
Bern <paul25bern@gmail.com>
2024-06-12 22:10:16 +0800

    testing access modifiers

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace seesharp_projects
 {
     class Program 
     {
+        private int numberId;
         static void Main(string[] args)
         {
             // Access Modifiers 
-
-        
+            numberId = new numberId() { Id = 1,} = 1;
+            
 
         }
     }
--- obj/Debug/net8.0/seesharp-projects.AssemblyInfo.cs
+++ obj/Debug/net8.0/seesharp-projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac4f662e9f909c18f81dd023c4bab2cd01da73f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53d460acf6b7ec5601c63cc8ff8d68bca033ac33")]
 [assembly: System.Reflection.AssemblyProductAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
