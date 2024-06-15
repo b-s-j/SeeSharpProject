6059b431bdf33fafe3ba4dc86a5678927b2b9d6f
Bern <paul25bern@gmail.com>
2024-06-15 23:53:44 +0800

    guessing game

--- Program.cs
+++ Program.cs
@@ -6,11 +6,38 @@ namespace seesharp_projects
 {
     class Program 
     {
-       static void Main(string[] args){
+       static void Main(string[] args)
+       {
+         Random random = new Random();
+        bool playAgain = true;
+        int min = 1; 
+        int max = 100;
+        int guess;
+        int number;
+        int guesses;
 
-        Messages.Hello();
-        Messages.Waiting();
-        Messages.Bye();
+        while (playAgain){
+          guess = 0;
+          guesses = 0;
+          number = random.Next(min, max + 1);
+
+          while(guess != number){
+           System.Console.Write($"Guess a number between  {min} - {max} : ");
+           guess = Convert.ToInt32(Console.ReadLine());
+           System.Console.WriteLine($"Guess: {guess}");
+
+           if (guess > number){
+            System.Console.WriteLine($"{guess} is too high!");
+           }else if(guess < number){
+            System.Console.WriteLine($"{guess} is too low!");
+           }
+           guesses++;
+          }
+          System.Console.WriteLine($"Number: {number}");
+          System.Console.WriteLine($"You Win!");
+          System.Console.WriteLine($"Guesses: {guesses}");
+        }
+        System.Console.WriteLine();
         
        }
     }
--- obj/Debug/net8.0/seesharp-projects.AssemblyInfo.cs
+++ obj/Debug/net8.0/seesharp-projects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51543b093f5c2f0ad957427962f833d532b303fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869a235b43e8308baed1277bb0f9854a030e86d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("seesharp-projects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
