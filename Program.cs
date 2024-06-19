d800f38bc44d0c130236120873a093fd6a63ff66
Bern <paul25bern@gmail.com>
2024-06-19 16:31:59 +0800

    Methods

--- Program.cs
+++ Program.cs
@@ -6,14 +6,14 @@ namespace seesharp_projects
 {
   class Program
   {
+
+    static void MyMethod(string fname)
+    {
+      Console.WriteLine("First name: " + fname);
+    }
     static void Main(string[] args)
     {
-      var num1 = 0b1001;
-      var num2 = 0b01000011;
-
-      System.Console.WriteLine($"Value of Num1 is: {num1}" );
-      System.Console.WriteLine($"Value of Num2 is: {num2}");
-      System.Console.WriteLine("Char value of Num2 is: {0}", Convert.ToChar(num2));
+      MyMethod("Lian");
     }
   }
 }
