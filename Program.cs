bf327b27b31d7b727b0b41b2a4000398dc6b9fa8
Bern <paul25bern@gmail.com>
2024-06-20 15:54:33 +0800

    getting ip address hostname of your local machine using c#

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Text;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
 
 namespace seesharp_projects
 {
   class Program
   {
 
-    static void MyMethod(string fname)
-    {
-      Console.WriteLine("First name: " + fname);
-    }
+    
     static void Main(string[] args)
     {
-      MyMethod("Lian");
+      // C# Program to find the ip address of the machine 
+      string hostname = Dns.GetHostName();
+      System.Console.WriteLine(hostname);
+
+      string IP = Dns.GetHostEntry(hostname).AddressList[0].ToString();
+      System.Console.WriteLine("Ip Address is: " + IP);
+      
     }
   }
 }
