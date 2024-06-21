9ef06275064ec00754223735bfca010e64a57ac2
Bern <paul25bern@gmail.com>
2024-06-21 17:10:35 +0800

    Environment usage

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 
+
 namespace seesharp_projects
 {
   class Program
@@ -12,13 +13,11 @@ namespace seesharp_projects
     
     static void Main(string[] args)
     {
-      // C# Program to find the ip address of the machine 
-      string hostname = Dns.GetHostName();
-      System.Console.WriteLine(hostname);
-
-      string IP = Dns.GetHostEntry(hostname).AddressList[0].ToString();
-      System.Console.WriteLine("Ip Address is: " + IP);
-      
+        Console.WriteLine($"Command Line: {Environment.CommandLine}");
+        Console.WriteLine($"Current Directory: {Environment.CurrentDirectory}");
+        Console.WriteLine($"Machine Name: {Environment.MachineName}");
+        Console.WriteLine($"OS Version: {Environment.OSVersion}");
+        Console.WriteLine($"User Name: {Environment.UserName}");
     }
   }
 }
